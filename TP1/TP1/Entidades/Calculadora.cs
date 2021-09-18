1d81569eb4abd9034ec06af69dcaf3385dfbc72f
Anton <aturavinin@gmail.com>
2021-09-18 15:08:57 -0300

    Se crean las dos clases de Entidades

---
+++ TP1/TP1/Entidades/Calculadora.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public static class Calculadora
+    {
+        private static char ValidarOperador(char operador)
+        {
+            char output = '+';
+            if (operador == '-' || operador == '/' || operador == '*')
+                output = operador;
+
+            return output;
+        }
+    }
+}
---
+++ TP1/TP1/Entidades/Operando.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entidades
+{
+    public class Operando
+    {
+        private double numero;
+        public string Numero
+        {
+            set
+            {
+                this.numero = ValidarOperando(value);
+            }
+        }
+
+        #region Constructores
+        public Operando() : this("0")
+        { }
+
+        public Operando(double numero) : this(numero.ToString())
+        { }
+
+        public Operando(string strNumero)
+        {
+            this.Numero = strNumero;
+        }
+        #endregion
+
+        #region Metodos
+        private double ValidarOperando(string strNumero)
+        {
+            double output = 0;
+            double auxDouble;
+            bool esNumValido;
+
+            esNumValido = double.TryParse(strNumero, out auxDouble);
+            if (esNumValido)
+                output = auxDouble;
+
+            return output;
+        }
+
+        private bool EsBinario(string binario)
+        {
+            bool output = true;
+            foreach (char charNumero in binario)
+            {
+                if (charNumero != '0' && charNumero != '1')
+                {
+                    output = false;
+                    break;
+                }
+            }
+            return output;
+        } 
+        #endregion
+    }
+}
