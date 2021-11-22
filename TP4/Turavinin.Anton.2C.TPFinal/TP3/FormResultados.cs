d1b2b860fd274f54c97cbe0a88c133b1e08cc749
Anton <aturavinin@gmail.com>
2021-11-22 16:27:01 -0300

    Elimina funcionalidad colores bar

--- TP4/Turavinin.Anton.2C.TPFinal/TP3/FormResultados.cs
+++ TP4/Turavinin.Anton.2C.TPFinal/TP3/FormResultados.cs
@@ -56,16 +56,4 @@ namespace TP3
 
  
     }
-
-
-    public static class ModifyProgressBarColor
-    {
-        [DllImport("user32.dll", CharSet = CharSet.Auto, SetLastError = false)]
-        static extern IntPtr SendMessage(IntPtr hWnd, uint Msg, IntPtr w, IntPtr l);
-
-        public static void SetState(this ProgressBar pBar, int state)
-        {
-            SendMessage(pBar.Handle, 1040, (IntPtr)state, IntPtr.Zero);
-        }
-    }
 }
